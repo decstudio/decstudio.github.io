d75e714ed58e9593ce1c66a6d380d32583c590cb
Dinnih <95191302+Dinnih@users.noreply.github.com>
2022-06-24 13:12:55 +0200

    Update Portfolio_Fumetto_Carmen.js

--- Portfolio_Fumetto_Carmen.js
+++ Portfolio_Fumetto_Carmen.js
@@ -2,13 +2,11 @@ var modal = document.getElementById("myModal");
 
         var img = document.getElementById("myImg");
         var modalImg = document.getElementById("img01");
-        var captionText = document.getElementById("caption");
-
-        function mostraImmagine(imageName, imageCaption) {
+       
+        function mostraImmagine(imageName) {
           modal.style.display = "block";
           modalImg.src = "Immagini/Fumetto_Carmen/Fumetto_Carmen_FullScreen/" + imageName;
-          captionText.innerHTML = imageCapt
-        }
+          }
 
         var span = document.getElementsByClassName("close")[0];
 
